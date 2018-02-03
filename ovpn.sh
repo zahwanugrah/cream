c7879decc17e6ffe9d07068beca0334d5d1769f4
brantbell <31989675+brantbell@users.noreply.github.com>
2018-02-03 22:13:31 +0800

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -37,7 +37,7 @@ vps="aneka";
 cd
 
 # check registered ip
-wget -q -O IP $source/brantbell/cream/mei/IP.txt
+wget -q -O IP https://raw.githubusercontent.com/brantbell/cream/mei/brantbell/cream/mei/IP.txt
 if ! grep -w -q $MYIP IP; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "zvur" ]]; then
@@ -54,14 +54,14 @@ apt-get -y --purge remove openvpn*;
 # install openvpn
 #myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar $source/brantbell/cream/mei/openvpn-debian.tar
+wget -O /etc/openvpn/openvpn.tar https://raw.githubusercontent.com/brantbell/cream/mei/brantbell/cream/mei/openvpn-debian.tar
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf $source/brantbell/cream/mei/1194.conf
+wget -O /etc/openvpn/1194.conf https://raw.githubusercontent.com/brantbell/cream/mei/brantbell/cream/mei/1194.conf
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf $source/cream/mei/iptables.conf
+wget -O /etc/iptables.conf https://raw.githubusercontent.com/brantbell/cream/mei/cream/mei/iptables.conf
 sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
 
 
@@ -72,7 +72,7 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn $source/brantbell/cream/mei/1194-client.conf
+wget -O /etc/openvpn/client.ovpn https://raw.githubusercontent.com/brantbell/cream/mei/brantbell/cream/mei/1194-client.conf
 
 cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
 sed -i 's/ipserver/$MYIP/g' /home/vps/public_html/client.ovpn
