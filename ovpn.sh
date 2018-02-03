c02a52f35d38f6e20c31c8bafb80dac75e3a2d7f
brantbell <31989675+brantbell@users.noreply.github.com>
2018-02-03 19:34:38 +0800

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -30,14 +30,14 @@ vps="aneka";
 #if [[ $vps = "zvur" ]]; then
 	#source="http://"
 #else
-	source="https://raw.githubusercontent.com/cy-ber/crime"
+	source="https://raw.githubusercontent.com/cream/mei"
 #fi
 
 # go to root
 cd
 
 # check registered ip
-wget -q -O IP $source/debian7/IP.txt
+wget -q -O IP $source/cream/mei/IP.txt
 if ! grep -w -q $MYIP IP; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "zvur" ]]; then
@@ -54,14 +54,14 @@ apt-get -y --purge remove openvpn*;
 # install openvpn
 #myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar $source/debian7/openvpn-debian.tar
+wget -O /etc/openvpn/openvpn.tar $source/cream/mei/openvpn-debian.tar
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf $source/debian7/1194.conf
+wget -O /etc/openvpn/1194.conf $source/cream/mei/1194.conf
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf $source/debian7/iptables.conf
+wget -O /etc/iptables.conf $source/cream/mei/iptables.conf
 sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
 
 
@@ -72,7 +72,7 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn $source/debian7/1194-client.conf
+wget -O /etc/openvpn/client.ovpn $source/cream/mei/1194-client.conf
 
 cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
 sed -i 's/ipserver/$MYIP/g' /home/vps/public_html/client.ovpn
