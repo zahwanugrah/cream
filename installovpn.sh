c8192cc4e4414e58dcf3b4a72923b1c60746a7b5
brantbell <31989675+brantbell@users.noreply.github.com>
2018-08-07 20:03:22 +0800

    Update installovpn.sh

--- installovpn.sh
+++ installovpn.sh
@@ -51,27 +51,130 @@ fi
 
 
 # install openvpn
-apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar https://raw.githubusercontent.com/brantbell/cream/mei/openvpn-debian.tar
-cd /etc/openvpn/
-tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf https://raw.githubusercontent.com/brantbell/cream/mei/1194.conf
-service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf https://raw.githubusercontent.com/brantbell/cream/mei/iptables.conf
-sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
+#apt-get install openvpn -y
+#wget -O /etc/openvpn/openvpn.tar https://raw.githubusercontent.com/brantbell/cream/mei/openvpn-debian.tar
+#cd /etc/openvpn/
+#tar xf openvpn.tar
+#wget -O /etc/openvpn/1194.conf https://raw.githubusercontent.com/brantbell/cream/mei/1194.conf
+#service openvpn restart
+#sysctl -w net.ipv4.ip_forward=1
+#sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+#wget -O /etc/iptables.conf https://raw.githubusercontent.com/brantbell/cream/mei/iptables.conf
+#sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
 
-myip2="s/ipserver/$MYIP/g";
-sed -i $myip2 /etc/iptables.conf;
+#myip2="s/ipserver/$MYIP/g";
+#sed -i $myip2 /etc/iptables.conf;
 
-iptables-restore < /etc/iptables.conf
-service openvpn restart
+#iptables-restore < /etc/iptables.conf
+#service openvpn restart
 
 # configure openvpn client config
-cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn https://raw.githubusercontent.com/brantbell/cream/mei/1194-client.conf
+#cd /etc/openvpn/
+#wget -O /etc/openvpn/client.ovpn https://raw.githubusercontent.com/brantbell/cream/mei/1194-client.conf
 
-cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
-sed -i $myip2 /home/vps/public_html/client.ovpn
-sed -i "s/ports/55/" /home/vps/public_html/client.ovpn
+#cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
+#sed -i $myip2 /home/vps/public_html/client.ovpn
+#sed -i "s/ports/55/" /home/vps/public_html/client.ovpn
+
+apt-get -y install openvpn easy-rsa openssl iptables
+cp -r /usr/share/easy-rsa/ /etc/openvpn
+mkdir /etc/openvpn/easy-rsa/keys
+# replace bits
+sed -i 's|export KEY_COUNTRY="US"|export KEY_COUNTRY="PH"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="Albay"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="Legazpi"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="IIEE"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="rdbtx123@gmail.com"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU="MyOrganizationalUnit"|export KEY_OU="daybreakersx"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_NAME="EasyRSA"|export KEY_NAME="daybreakersx"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU=changeme|export KEY_OU=daybreakersx|' /etc/openvpn/easy-rsa/vars
+#Create Diffie-Helman Pem
+openssl dhparam -out /etc/openvpn/dh2048.pem 2048
+# Create PKI
+cd /etc/openvpn/easy-rsa
+. ./vars
+./clean-all
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --initca $*
+# create key server
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --server server
+# setting KEY CN
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" client
+cd
+#cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key,ca.crt} /etc/openvpn
+cp /etc/openvpn/easy-rsa/keys/server.crt /etc/openvpn/server.crt
+cp /etc/openvpn/easy-rsa/keys/server.key /etc/openvpn/server.key
+cp /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn/ca.crt
+# Setting Server
+cat > /etc/openvpn/server.conf <<-END
+port 1194
+proto tcp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+client-cert-not-required
+username-as-common-name
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+server 192.168.100.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "route-method exe"
+push "route-delay 2"
+duplicate-cn
+push "route-method exe"
+push "route-delay 2"
+keepalive 10 120
+comp-lzo
+user nobody
+group nogroup
+persist-key
+persist-tun
+status openvpn-status.log
+log         openvpn.log
+verb 3
+cipher AES-128-CBC
+END
+
+#Create OpenVPN Config
+mkdir -p /home/vps/public_html
+cat > /home/vps/public_html/client.ovpn <<-END
+# Modified by DENBAGUSS
+client
+dev tun
+proto tcp
+remote $MYIP 1194
+persist-key
+persist-tun
+dev tun
+pull
+resolv-retry infinite
+nobind
+user nobody
+group nogroup
+comp-lzo
+ns-cert-type server
+verb 3
+mute 2
+mute-replay-warnings
+auth-user-pass
+redirect-gateway def1
+script-security 2
+route 0.0.0.0 0.0.0.0
+route-method exe
+route-delay 2
+cipher AES-128-CBC
+http-proxy $MYIP 8080
+http-proxy-retry
+END
+echo '<ca>' >> /home/vps/public_html/client.ovpn
+cat /etc/openvpn/ca.crt >> /home/vps/public_html/client.ovpn
+echo '</ca>' >> /home/vps/public_html/client.ovpn
+cd /home/vps/public_html/
+tar -czf /home/vps/public_html/openvpn.tar.gz client.ovpn
+tar -czf /home/vps/public_html/client.tar.gz client.ovpn
