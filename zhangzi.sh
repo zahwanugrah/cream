d1fa4432fb7edebb1306fadf652eca7caac83940
brantbell <31989675+brantbell@users.noreply.github.com>
2018-09-26 11:51:54 +0800

    Update zhangzi.sh

--- zhangzi.sh
+++ zhangzi.sh
@@ -283,7 +283,7 @@ http_access deny manager
 http_access allow localhost
 http_access deny all
 http_port 8080
-http_port 81
+http_port 80
 http_port 3128
 coredump_dir /var/spool/squid3
 refresh_pattern ^ftp: 1440 20% 10080
