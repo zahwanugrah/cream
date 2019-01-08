f11bd3ff7d2f905176766659e60f53ac2994adb9
brantbell <31989675+brantbell@users.noreply.github.com>
2019-01-08 20:56:50 +0700

    Update zhangzi.sh

--- zhangzi.sh
+++ zhangzi.sh
@@ -86,12 +86,20 @@ ln -fs /usr/share/zoneinfo/Asia/Indonesia /etc/localtime
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
+
 # set repo
-wget -O /etc/apt/sources.list https://raw.githubusercontent.com/brantbell/cream/mei/sources.list.debian7
-wget http://www.dotdeb.org/dotdeb.gpg
-wget http://www.webmin.com/jcameron-key.asc
+cat > /etc/apt/sources.list <<END2
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb-src http://security.debian.org/ jessie/updates main contrib non-free
+deb http://http.us.debian.org/debian jessie main contrib non-free
+deb http://packages.dotdeb.org jessie all
+deb-src http://packages.dotdeb.org jessie all
+END2
+wget "http://www.dotdeb.org/dotdeb.gpg"
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
-cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
+
+
+
 
 # remove unused
 apt-get -y --purge remove samba*;
