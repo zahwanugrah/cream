249405270ddd413a80d11b5a6409f73946a0ef3e
emue25 <48172339+emue25@users.noreply.github.com>
2019-09-17 10:35:52 +0800

    Update deb8.sh

--- deb8.sh
+++ deb8.sh
@@ -263,30 +263,30 @@ service webmin restart
 #service webmin restart
 
 #install PPTP
-apt-get -y install pptpd
-cat > /etc/ppp/pptpd-options <<END
-name pptpd
-refuse-pap
-refuse-chap
-refuse-mschap
-require-mschap-v2
-require-mppe-128
-ms-dns 8.8.8.8
-ms-dns 8.8.4.4
-proxyarp
-nodefaultroute
-lock
-nobsdcomp
-END
-echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
-echo "logwtmp" >> /etc/pptpd.conf
-echo "localip 10.1.0.1" >> /etc/pptpd.conf
-echo "remoteip 10.1.0.5-100" >> /etc/pptpd.conf
-cat >> /etc/ppp/ip-up <<END
-ifconfig ppp0 mtu 1400
-END
-mkdir /var/lib/premium-script
-/etc/init.d/pptpd restart
+#apt-get -y install pptpd
+#cat > /etc/ppp/pptpd-options <<END
+#name pptpd
+#refuse-pap
+#refuse-chap
+#refuse-mschap
+#require-mschap-v2
+#require-mppe-128
+#ms-dns 8.8.8.8
+#ms-dns 8.8.4.4
+#proxyarp
+#nodefaultroute
+#lock
+#nobsdcomp
+#END
+#echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
+#echo "logwtmp" >> /etc/pptpd.conf
+#echo "localip 10.1.0.1" >> /etc/pptpd.conf
+#echo "remoteip 10.1.0.5-100" >> /etc/pptpd.conf
+#cat >> /etc/ppp/ip-up <<END
+#ifconfig ppp0 mtu 1400
+#END
+#mkdir /var/lib/premium-script
+#/etc/init.d/pptpd restart
 
 # install mrtg
 #wget -O /etc/snmp/snmpd.conf "https://raw.githubusercontent.com/emue25/cream/mei/snmpd.conf"
