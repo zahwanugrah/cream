6cd90d82f9731443c605830f9da74950fea9e943
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-03 09:32:01 +0800

    Update zhangzi_kusus_ovpn_dan_ovpn_ssl.sh

--- zhangzi_kusus_ovpn_dan_ovpn_ssl.sh
+++ zhangzi_kusus_ovpn_dan_ovpn_ssl.sh
@@ -213,9 +213,9 @@ refresh_pattern ^ftp: 1440 20% 10080
 refresh_pattern ^gopher: 1440 0% 1440
 refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
 refresh_pattern . 0 20% 4320
-visible_hostname daybreakersx
+visible_hostname kopet
 END
-sed -i $MYIP2 /etc/squid3/squid.conf.bak;
+sed -i $MYIP2 /etc/squid3/squid.conf;
 /etc/init.d/squid3 restart
 
 # install stunnel4
