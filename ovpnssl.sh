53799b85b34908d89c44f69c9c8399967b32c544
emue25 <48172339+emue25@users.noreply.github.com>
2019-10-16 08:52:32 +0800

    Update ovpnssl.sh

--- ovpnssl.sh
+++ ovpnssl.sh
@@ -128,7 +128,7 @@ http {
 }
 END3
 mkdir -p /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://shortenerku.com/"
+wget -O /home/vps/public_html/index.html "https://sshfast.net/"
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 args='$args'
 uri='$uri'
