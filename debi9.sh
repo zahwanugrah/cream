4792f5dd43a354bdd032590b8940cebd31aabd76
emue25 <48172339+emue25@users.noreply.github.com>
2019-12-13 10:08:05 +0800

    Update debi9.sh

--- debi9.sh
+++ debi9.sh
@@ -139,7 +139,7 @@ http_access allow localhost
 http_access deny all
 http_port 8080
 http_port 8000
-http_port 80
+http_port 81
 http_port 3128
 coredump_dir /var/spool/squid3
 refresh_pattern ^ftp: 1440 20% 10080
@@ -240,6 +240,41 @@ cipher none
 auth none
 END
 systemctl start openvpn@server.service
+#vpn2
+cat > /etc/openvpn/server2.conf <<-END
+port 55
+proto tcp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh1024.pem
+verify-client-cert none
+username-as-common-name
+plugin /usr/lib/openvpn/plugins/openvpn-plugin-auth-pam.so login
+server 192.168.100.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "route-method exe"
+push "route-delay 2"
+socket-flags TCP_NODELAY
+push "socket-flags TCP_NODELAY"
+keepalive 10 120
+comp-lzo
+user nobody
+group nogroup
+persist-key
+persist-tun
+status openvpn-status.log
+log openvpn.log
+verb 3
+ncp-disable
+cipher none
+auth none
+END
+systemctl start openvpn@server2.service
 #Create OpenVPN Config
 mkdir -p /home/vps/public_html
 cat > /home/vps/public_html/zhangzi.ovpn <<-END
@@ -250,7 +285,7 @@ client
 dev tun
 proto tcp
 remote $MYIP 443
-http-proxy $MYIP 80
+http-proxy $MYIP 8080
 persist-key
 persist-tun
 pull
@@ -324,7 +359,7 @@ sudo touch stunnel.conf
 echo "client = no" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "[openvpn]" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "accept = 444" | sudo tee -a /etc/stunnel/stunnel.conf
-echo "connect = $MYIP:443" | sudo tee -a /etc/stunnel/stunnel.conf
+echo "connect = $MYIP:55" | sudo tee -a /etc/stunnel/stunnel.conf
 echo "cert = /etc/stunnel/stunnel.pem" | sudo tee -a /etc/stunnel/stunnel.conf
 sudo sed -i -e 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 iptables -A INPUT -p tcp --dport 444 -j ACCEPT
@@ -336,6 +371,7 @@ END
 /etc/init.d/stunnel4 restart
 
 ufw allow ssh
+ufw allow 55/tcp
 ufw allow 443/tcp
 sed -i 's|DEFAULT_INPUT_POLICY="DROP"|DEFAULT_INPUT_POLICY="ACCEPT"|' /etc/default/ufw
 sed -i 's|DEFAULT_FORWARD_POLICY="DROP"|DEFAULT_FORWARD_POLICY="ACCEPT"|' /etc/default/ufw
@@ -353,6 +389,7 @@ cat > /etc/iptables.up.rules <<-END
 -A POSTROUTING -j SNAT --to-source xxxxxxxxx
 -A POSTROUTING -o eth0 -j MASQUERADE
 -A POSTROUTING -s 192.168.10.0/24 -o eth0 -j MASQUERADE
+-A POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 COMMIT
 *filter
 :INPUT ACCEPT [0:0]
