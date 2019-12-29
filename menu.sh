472d6d7b7c36371dae9d4e35068630d23391451f
emue25 <48172339+emue25@users.noreply.github.com>
2019-12-29 14:21:34 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -37,9 +37,9 @@ cat /usr/bin/bannermenu
 echo "                    Server: $MYIP"  | lolcat
 date +"                    %A, %d-%m-%Y"  | lolcat
 date +"                            %H:%M:%S %Z" | lolcat
-echo "" | lolcat
-echo "" | lolcat
-PS3='Silahkan ketik nomor pilihan anda lalu tekan ENTER: | lolcat
+echo "" 
+echo "" 
+PS3='Silahkan ketik nomor pilihan anda lalu tekan ENTER:
 options=("Buat User SSH/OVPN" "Buat User SSH/OVPN Trial" "Perbarui User" "Ganti Password User SSH/OVPN" "Semua User Dan Tanggal Kadaluarsa" "Hapus User" "Buat User PPTP VPN" "Monitor User Login" "Daftar User Aktif" "Daftar User Kadaluarsa" "Disable User Kadaluarsa" "Hapus User Kadaluarsa" "Banned User" "Unbanned User" "Penggunaan Ram" "Speedtest" "Benchmark" "Manual Kill Multi Login" "(ON)AutoKill Multi Login" "(OFF)AutoKill Multi Login" "Ganti Password VPS" "Bersihkan Cache Ram Manual" "Edit Banner Login" "Edit Banner Menu" "Lihat Lokasi User" "Restart Webmin" "Restart Server VPS" "Restart Dropbear" "Restart OpenSSH" "Restart Squid3" "Restart OpenVPN" "Restart SSL" "Ganti Port OpenSSH" "Ganti Port Dropbear" "Ganti Port Squid3" "Ganti Port OpenVPN" "Update Script VPS" "Quit")
 select opt in "${options[@]}"
 do
