22855e6c88ca4af8eab7298ba7166ec14b9e5559
emue25 <48172339+emue25@users.noreply.github.com>
2019-12-29 13:08:53 +0800

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -12,36 +12,36 @@ cd
 
 # download script
 cd
-wget -O /usr/bin/benchmark "https://raw.githubusercontent.com/brantbell/cream/mei/benchmark.sh"
-wget -O /usr/bin/speedtest "https://raw.githubusercontent.com/emue25/VPSauto/master/speedtest_cli.py"
-wget -O /usr/bin/ps-mem "https://raw.githubusercontent.com/brantbell/cream/mei/ps_mem.py"
-wget -O /usr/bin/dropmon "https://raw.githubusercontent.com/brantbell/cream/mei/dropmon.sh"
-wget -O /usr/bin/menu "https://raw.githubusercontent.com/brantbell/cream/mei/menu.sh"
-wget -O /usr/bin/user-active-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-active-list.sh"
-wget -O /usr/bin/user-add "https://raw.githubusercontent.com/emue25/cream/mei/user-add.sh"
-wget -O /usr/bin/user-add-pptp "https://raw.githubusercontent.com/brantbell/cream/mei/user-add-pptp.sh"
-wget -O /usr/bin/user-del "https://raw.githubusercontent.com/brantbell/cream/mei/user-del.sh"
-wget -O /usr/bin/disable-user-expire "https://raw.githubusercontent.com/brantbell/cream/mei/disable-user-expire.sh"
-wget -O /usr/bin/delete-user-expire "https://raw.githubusercontent.com/brantbell/cream/mei/delete-user-expire.sh"
-wget -O /usr/bin/banned-user "https://raw.githubusercontent.com/brantbell/cream/mei/banned-user.sh"
-wget -O /usr/bin/unbanned-user "https://raw.githubusercontent.com/brantbell/cream/mei/unbanned-user.sh"
-wget -O /usr/bin/user-expire-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-expire-list.sh"
-wget -O /usr/bin/user-gen "https://raw.githubusercontent.com/brantbell/cream/mei/user-gen.sh"
-wget -O /usr/bin/userlimit.sh "https://raw.githubusercontent.com/brantbell/cream/mei/userlimit.sh"
+wget -O /usr/bin/benchmark "https://raw.githubusercontent.com/brantbell/cream/mei/benchmark.sh"| lolcat
+wget -O /usr/bin/speedtest "https://raw.githubusercontent.com/emue25/VPSauto/master/speedtest_cli.py"| lolcat
+wget -O /usr/bin/ps-mem "https://raw.githubusercontent.com/brantbell/cream/mei/ps_mem.py"| lolcat
+wget -O /usr/bin/dropmon "https://raw.githubusercontent.com/brantbell/cream/mei/dropmon.sh"| lolcat
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/brantbell/cream/mei/menu.sh"| lolcat
+wget -O /usr/bin/user-active-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-active-list.sh"| lolcat
+wget -O /usr/bin/user-add "https://raw.githubusercontent.com/emue25/cream/mei/user-add.sh"| lolcat
+wget -O /usr/bin/user-add-pptp "https://raw.githubusercontent.com/brantbell/cream/mei/user-add-pptp.sh"| lolcat
+wget -O /usr/bin/user-del "https://raw.githubusercontent.com/brantbell/cream/mei/user-del.sh"| lolcat
+wget -O /usr/bin/disable-user-expire "https://raw.githubusercontent.com/brantbell/cream/mei/disable-user-expire.sh"| lolcat
+wget -O /usr/bin/delete-user-expire "https://raw.githubusercontent.com/brantbell/cream/mei/delete-user-expire.sh"| lolcat
+wget -O /usr/bin/banned-user "https://raw.githubusercontent.com/brantbell/cream/mei/banned-user.sh"| lolcat
+wget -O /usr/bin/unbanned-user "https://raw.githubusercontent.com/brantbell/cream/mei/unbanned-user.sh"| lolcat
+wget -O /usr/bin/user-expire-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-expire-list.sh"| lolcat
+wget -O /usr/bin/user-gen "https://raw.githubusercontent.com/brantbell/cream/mei/user-gen.sh"| lolcat
+wget -O /usr/bin/userlimit.sh "https://raw.githubusercontent.com/brantbell/cream/mei/userlimit.sh"| lolcat
 #wget -O /usr/bin/userlimitssh.sh "https://raw.githubusercontent.com/brantbell/cream/mei/userlimitssh.sh"
-wget -O /usr/bin/user-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-list.sh"
-wget -O /usr/bin/user-login "https://raw.githubusercontent.com/brantbell/cream/mei/user-login.sh"
-wget -O /usr/bin/user-pass "https://raw.githubusercontent.com/brantbell/cream/mei/user-pass.sh"
-wget -O /usr/bin/user-renew "https://raw.githubusercontent.com/brantbell/cream/mei/user-renew.sh"
-wget -O /usr/bin/clearcache.sh "https://raw.githubusercontent.com/brantbell/cream/mei/clearcache.sh"
-wget -O /usr/bin/bannermenu "https://raw.githubusercontent.com/brantbell/cream/mei/bannermenu"
-wget -O /usr/bin/menu-update-script-vps.sh "https://raw.githubusercontent.com/emue25/cream/mei/menu-update-script-vps.sh"
-wget -O /usr/bin/vpnmon "https://raw.githubusercontent.com/brantbell/cream/mei/vpnmon"
+wget -O /usr/bin/user-list "https://raw.githubusercontent.com/brantbell/cream/mei/user-list.sh"| lolcat
+wget -O /usr/bin/user-login "https://raw.githubusercontent.com/brantbell/cream/mei/user-login.sh"| lolcat
+wget -O /usr/bin/user-pass "https://raw.githubusercontent.com/brantbell/cream/mei/user-pass.sh"| lolcat
+wget -O /usr/bin/user-renew "https://raw.githubusercontent.com/brantbell/cream/mei/user-renew.sh"| lolcat
+wget -O /usr/bin/clearcache.sh "https://raw.githubusercontent.com/brantbell/cream/mei/clearcache.sh"| lolcat
+wget -O /usr/bin/bannermenu "https://raw.githubusercontent.com/brantbell/cream/mei/bannermenu"| lolcat
+wget -O /usr/bin/menu-update-script-vps.sh "https://raw.githubusercontent.com/emue25/cream/mei/menu-update-script-vps.sh"| lolcat
+wget -O /usr/bin/vpnmon "https://raw.githubusercontent.com/brantbell/cream/mei/vpnmon"| lolcat
 
-wget -O /usr/bin/edit-port-openssh "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-openssh"
-wget -O /usr/bin/edit-port-openvpn "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-openvpn"
-wget -O /usr/bin/edit-port-squid "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-squid"
-wget -O /usr/bin/edit-port-dropbear "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-dropbear"
+wget -O /usr/bin/edit-port-openssh "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-openssh"| lolcat
+wget -O /usr/bin/edit-port-openvpn "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-openvpn"| lolcat
+wget -O /usr/bin/edit-port-squid "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-squid"| lolcat
+wget -O /usr/bin/edit-port-dropbear "https://raw.githubusercontent.com/brantbell/cream/mei/edit-port-dropbear"| lolcat
 
 
 cd
@@ -87,5 +87,5 @@ echo " "
 echo "Premium Script Successfully Installed!"
 echo "Restarting all services..."
 echo "Wait for a few minutes..."
-echo "Modified by DENBAGUSS"
+echo "Modified by denbaguss"
 echo "Create by https://vpnstunnel.com"
