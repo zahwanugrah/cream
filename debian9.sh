974dc788300ed66ee59d28180ac46bde2ffffa01
emue25 <48172339+emue25@users.noreply.github.com>
2020-04-25 10:46:39 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -554,7 +554,7 @@ http {
 }
 END3
 mkdir -p /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://sshfast.net/"
+wget -O /home/vps/public_html/index.html "https://vpnshock.com/"
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 args='$args'
 uri='$uri'
