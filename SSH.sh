ef86f49a6db2776dde1c2aecc25e45d456644a1b
emue25 <48172339+emue25@users.noreply.github.com>
2020-10-24 13:30:15 +0800

    Update SSH.sh

--- SSH.sh
+++ SSH.sh
@@ -122,7 +122,7 @@ chown -R vnstat:vnstat /var/lib/vnstat
 # install squid3
 cd
 apt-get -y install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/squid3.conf"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/emue25/cream/mei/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf;
 /etc/init.d/squid restart
 
