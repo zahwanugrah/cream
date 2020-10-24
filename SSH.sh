3e8fe470362f1a6e0415aafe8725bf645dc8dd5d
emue25 <48172339+emue25@users.noreply.github.com>
2020-10-24 12:32:53 +0800

    Update SSH.sh

--- SSH.sh
+++ SSH.sh
@@ -261,7 +261,7 @@ chmod +x /usr/bin/build
 chmod +x /etc/rc.local
 
 # Custom Banner SSH
-wget -O /etc/issue.net "https://github.com/idtunnel/sshtunnel/raw/master/debian9/banner-custom.conf"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/emue25/cream/mei/bannerssh"
 chmod +x /etc/issue.net
 
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
