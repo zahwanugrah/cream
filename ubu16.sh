7841d954339c7cb7f09659bbfb334bda7a0aa908
emue25 <48172339+emue25@users.noreply.github.com>
2020-10-27 22:30:59 +0800

    Update ubu16.sh

--- ubu16.sh
+++ ubu16.sh
@@ -64,37 +64,6 @@ sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dr
 # install webserver
 cd
 sudo apt-get -y install nginx
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/nginx-default.conf"
-mkdir -p /home/vps/public_html
-echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/vhost-nginx.conf"
-/etc/init.d/nginx restart
-
-# instal nginx php5.6 
-apt-get -y install nginx php5.6-fpm
-apt-get -y install nginx php5.6-cli
-apt-get -y install nginx php5.6-mysql
-apt-get -y install nginx php5.6-mcrypt
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/5.6/cli/php.ini
-
-# cari config php fpm dengan perintah berikut "php --ini |grep Loaded"
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/5.6/cli/php.ini
-
-# Cari config php fpm www.conf dengan perintah berikut "find / \( -iname "php.ini" -o -name "www.conf" \)"
-sed -i 's/listen = \/run\/php\/php5.6-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/5.6/fpm/pool.d/www.conf
-cd
-
-
-# Edit port apache2 ke 8090
-wget -O /etc/apache2/ports.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/apache2.conf"
-
-# Edit port virtualhost apache2 ke 8090
-wget -O /etc/apache2/sites-enabled/000-default.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/virtualhost.conf"
-
-# restart apache2
-/etc/init.d/apache2 restart
 
 # openvpn
 cp -r /usr/share/easy-rsa/ /etc/openvpn
