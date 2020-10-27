d6f09a88612c79f602a4eeaf6ce8dedf83c89aee
emue25 <48172339+emue25@users.noreply.github.com>
2020-10-27 22:27:33 +0800

    Update ubu16.sh

--- ubu16.sh
+++ ubu16.sh
@@ -60,43 +60,6 @@ sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dr
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 
-# install badvpn
-cd /usr/bin
-mkdir build
-cd build
-wget https://github.com/ambrop72/badvpn/archive/1.999.130.tar.gz
-tar xvzf 1.999.130.tar.gz
-cd badvpn-1.999.130
-cmake -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1
-make install
-make -i install
-
-# auto start badvpn single port
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 20 &
-cd
-
-# auto start badvpn second port
-#cd /usr/bin/build/badvpn-1.999.130
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500 --max-connections-for-client 20 &
-cd
-
-# auto start badvpn second port
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500 --max-connections-for-client 20 &
-cd
-
-# permition
-chmod +x /usr/local/bin/badvpn-udpgw
-chmod +x /usr/local/share/man/man7/badvpn.7
-chmod +x /usr/local/bin/badvpn-tun2socks
-chmod +x /usr/local/share/man/man8/badvpn-tun2socks.8
-chmod +x /usr/bin/build
-chmod +x /etc/rc.local
-sudo apt update -y
-sudo apt install php5.6 -y
-sudo apt install php5.6-mcrypt php5.6-mysql php5.6-fpm php5.6-cli php5.6-common php5.6-curl php5.6-mbstring php5.6-mysqlnd php5.6-xml -y
 
 # install webserver
 cd
