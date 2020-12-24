7b824770a983a18b7169aedcb841ab1dba702a09
emue25 <48172339+emue25@users.noreply.github.com>
2020-12-24 17:48:48 +0800

    Update SSH.sh

--- SSH.sh
+++ SSH.sh
@@ -279,6 +279,195 @@ chmod +x /usr/local/bin/badvpn-tun2socks
 chmod +x /usr/local/share/man/man8/badvpn-tun2socks.8
 chmod +x /usr/bin/build
 chmod +x /etc/rc.local
+#Setting IPtables
+cat > /etc/iptables.up.rules <<-END
+*nat
+:PREROUTING ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+:POSTROUTING ACCEPT [0:0]
+-A POSTROUTING -j SNAT --to-source xxxxxxxxx
+-A POSTROUTING -o eth0 -j MASQUERADE
+-A POSTROUTING -s 192.168.10.0/24 -o eth0 -j MASQUERADE
+-A POSTROUTING -s 192.168.100.0/24 -o eth1 -j MASQUERADE
+-A POSTROUTING -s 192.168.200.0/24 -o eth0 -j MASQUERADE
+-A POSTROUTING -s 192.168.20.0/24 -o eth1 -j MASQUERADE
+COMMIT
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+:fail2ban-ssh - [0:0]
+-A INPUT -p tcp -m multiport --dports 22 -j fail2ban-ssh
+-A INPUT -p ICMP --icmp-type 8 -j ACCEPT
+-A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
+-A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 143  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 442  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 443  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 444  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 587  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 1194  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 1194  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 55  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 55  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 8085  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 8085  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 8888  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 8888  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 3128  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 3128  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 8080  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 8080  -m state --state NEW -j ACCEPT 
+-A INPUT -p tcp --dport 7300  -m state --state NEW -j ACCEPT
+-A INPUT -p udp --dport 7300  -m state --state NEW -j ACCEPT 
+-A INPUT -p tcp --dport 10000  -m state --state NEW -j ACCEPT
+-A INPUT -p tcp --dport 587 -j ACCEPT
+-A OUTPUT -p tcp --dport 6881:6889 -j DROP
+-A OUTPUT -p udp --dport 1024:65534 -j DROP
+-A FORWARD -m string --string "get_peers" --algo bm -j DROP
+-A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+-A FORWARD -m string --string "find_node" --algo bm -j DROP
+-A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+-A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+-A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+-A FORWARD -m string --algo bm --string ".torrent" -j DROP
+-A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+-A FORWARD -m string --algo bm --string "torrent" -j DROP
+-A FORWARD -m string --algo bm --string "announce" -j DROP
+-A FORWARD -m string --algo bm --string "info_hash" -j DROP
+-A fail2ban-ssh -j RETURN
+COMMIT
+*raw
+:PREROUTING ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+COMMIT
+
+## Allowing all neede ports for OpenVPN L2TP PPTP and RADIUS
+iptables -I OUTPUT -p udp --dport 1812 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 1813 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 1701 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 4500 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 500 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp --dport 1723 -j ACCEPT
+## Drop torrents
+iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
+iptables -A OUTPUT -p udp --dport 1024:65534 -j DROP
+## Set more radius (alt) ports
+iptables -I OUTPUT -p udp --dport 1645 -j ACCEPT
+iptables -I OUTPUT -p udp --dport 1646 -j ACCEPT
+## Try torrent name filters
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+## Trying forward
+iptables -A FORWARD -p tcp --dport 6881:6889 -j DROP
+iptables -A FORWARD -p udp --dport 1024:65534 -j DROP
+###### Found on web
+iptables -N LOGDROP > /dev/null 2> /dev/null
+iptables -F LOGDROP
+iptables -A LOGDROP -j DROP
+#Torrent
+iptables -D FORWARD -m string --algo bm --string "BitTorrent" -j LOGDROP 
+iptables -D FORWARD -m string --algo bm --string "BitTorrent protocol" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "peer_id=" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string ".torrent" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "announce.php?passkey=" -j LOGDROP 
+iptables -D FORWARD -m string --algo bm --string "torrent" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "announce" -j LOGDROP
+iptables -D FORWARD -m string --algo bm --string "info_hash" -j LOGDROP
+# DHT keyword
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j LOGDROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j LOGDROP
+#play
+iptables -A OUTPUT -d account.sonyentertainmentnetwork.com -j DROP
+iptables -A OUTPUT -d auth.np.ac.playstation.net -j DROP
+iptables -A OUTPUT -d auth.api.sonyentertainmentnetwork.com -j DROP
+iptables -A OUTPUT -d auth.api.np.ac.playstation.net -j DROP
+## Modified debia commands
+iptables -A FORWARD -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -A FORWARD -p udp -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "info_hash" -j DROP
+iptables -A FORWARD -p udp -m string --algo bm --string "tracker" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -A INPUT -p udp -m string --algo bm --string "torrent" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "info_hash" -j DROP
+iptables -A INPUT -p udp -m string --algo bm --string "tracker" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "torrent" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "info_hash" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "tracker" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D INPUT -p udp -m string --algo bm --string "torrent" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "info_hash" -j DROP
+iptables -D INPUT -p udp -m string --algo bm --string "tracker" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "peer_id=" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string ".torrent" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -I OUTPUT -p udp -m string --algo bm --string "torrent" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "info_hash" -j DROP
+iptables -I OUTPUT -p udp -m string --algo bm --string "tracker" -j DROP
+## Delete
+iptables -D INPUT -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D INPUT -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D INPUT -m string --algo bm --string "peer_id=" -j DROP
+iptables -D INPUT -m string --algo bm --string ".torrent" -j DROP
+iptables -D INPUT -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D INPUT -m string --algo bm --string "torrent" -j DROP
+iptables -D INPUT -m string --algo bm --string "announce" -j DROP
+iptables -D INPUT -m string --algo bm --string "info_hash" -j DROP
+iptables -D INPUT -m string --algo bm --string "tracker" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "peer_id=" -j DROP
+iptables -D OUTPUT -m string --algo bm --string ".torrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D OUTPUT -m string --algo bm --string "torrent" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "announce" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "info_hash" -j DROP
+iptables -D OUTPUT -m string --algo bm --string "tracker" -j DROP
+iptables -D FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -D FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -D FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP 
+iptables -D FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -D FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -D FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables -D FORWARD -m string --algo bm --string "tracker" -j DROP  
+*mangle
+:PREROUTING ACCEPT [0:0]
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+:POSTROUTING ACCEPT [0:0]
+COMMIT
+END
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
 
 # Custom Banner SSH
 wget -O /etc/issue.net "https://raw.githubusercontent.com/emue25/cream/mei/bannerssh"
